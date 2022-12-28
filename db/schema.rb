5fa2d28bb35b62c0b278d74e5e190bd2157e13c6
EC2 Default User <specialized.19991129@gmail.com>
2022-12-28 12:43:03 +0900

    [Add]articlesにuser_idカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_28_033640) do
+ActiveRecord::Schema.define(version: 2022_12_28_034143) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2022_12_28_033640) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "post_image"
+    t.integer "user_id"
   end
 
   create_table "comments", force: :cascade do |t|
