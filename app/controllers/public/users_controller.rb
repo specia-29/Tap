44b226a248707b1946810b8e6e8395b858b409b4
EC2 Default User <specialized.19991129@gmail.com>
2023-01-03 17:49:24 +0900

    [Update]プロフィール編集画面

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,13 +1,16 @@
 class Public::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    # @profile_image = @user.profile_image
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to public_user_path(@user.id)
   end
 
   def followed
@@ -17,10 +20,11 @@ class Public::UsersController < ApplicationController
   end
 
   def stat_update
+    @user = current_user
   end
 
   private
   def user_params
-    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :user_name, :birth_date, :phone_number, :is_deleted, :profile_image)
+    params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :encrypted_password, :user_name, :birth_date, :phone_number, :is_deleted, :profile_image, :introduction)
   end
 end
