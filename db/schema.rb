cab2fab41469994da96e22b33dfdf55ad3cc7275
EC2 Default User <specialized.19991129@gmail.com>
2023-01-17 11:19:27 +0900

    [Delete]commentsテーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_16_064016) do
+ActiveRecord::Schema.define(version: 2023_01_17_021802) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -62,14 +62,6 @@ ActiveRecord::Schema.define(version: 2023_01_16_064016) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "post_image"
-    t.integer "user_id"
-  end
-
-  create_table "comments", force: :cascade do |t|
-    t.string "comment"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "article_id"
     t.integer "user_id"
   end
 
