04c7a0dbaef8df462bb5d4942a90b880f9d60d0c
EC2 Default User <specialized.19991129@gmail.com>
2023-01-20 12:15:00 +0900

    [Update]ゲストログインrootingエラー解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -42,20 +42,18 @@ class User < ApplicationRecord
     self.favorites.exists?(article_id: article.id)
   end
 
-  def self.guest
-    find_or_create_by!(email: 'guest@example.com') do |user|
-      user.password = SecureRandom.urlsafe_base64
-      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
-      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
-    end
-  end
-
 # 検索方法分岐
   def self.looks(search, word)
     if search == "partial_match"
       @user = User.where("user_name LIKE?","%#{word}%")
     else
       @user = User.all
+    end
+  end
+
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,10 @@ Rails.application.routes.draw do
 
   root 'public/articles#index'
 
-  namespace :admin do
-    get 'articles/show'
-    get 'articles/edit'
+  devise_scope :user do
+    post 'public/guest_sign_in', to: 'public/sessions#guest_sign_in'
   end
-  devise_scope :public do
-    post 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
-    get 'users/guest_sign_in', to: 'public/sessions#guest_sign_in'
-  end
-  # 管理者用
-  # URL /admin/sign_in ...
+
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
     sessions: "admin/sessions"
 }
@@ -23,8 +17,8 @@ Rails.application.routes.draw do
     resources :articles, only: [:edit, :show, :update, :destroy]
   end
 
-  # 顧客用
-  # URL /customers/sign_in ...
+
+
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
