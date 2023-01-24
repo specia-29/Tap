a465af2da0771baf91af999eaa6e598daa739377
EC2 Default User <specialized.19991129@gmail.com>
2023-01-24 17:13:38 +0900

    [Finish]フォロー機能実装

--- test/controllers/relationships_controller_test.rb
+++ test/controllers/relationships_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class Public::RelationshipsControllerTest < ActionDispatch::IntegrationTest
+class RelationshipsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
